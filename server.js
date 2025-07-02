b87a4a6246b87c04bbc6ff22848af9a8e06e7159
mudasarsafdar09 <mudasar077689@gmail.com>
2025-07-03 03:17:59 +0500

    initial frontend commit

--- server.js
+++ server.js
@@ -22,6 +22,5 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/meetings", meetingRoutes);
 
-
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
